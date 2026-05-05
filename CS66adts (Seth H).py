2eeaec7592e60adefc94ed78c285b7753db8ca87
Seth Hall <seth.m.hall07@gmail.com>
2026-05-05 18:14:04 -0500

    Add files via upload

--- CS66adts (Seth H).py
+++ CS66adts (Seth H).py
@@ -1,5 +1,10 @@
 import random
+import json
 
+with open("loot_table.json", 'r') as f:
+    loot_table = json.load(f)
+
+#ADT for the combat system
 class Combat:
     def __init__(self):
         self.temproll = 0 #placeholder for the 20 sided-dice rolls
@@ -10,19 +15,22 @@ class Combat:
     def roll_6(self): # A 6 sided dice to determine move order for the fight
         self.temproll = random.randint(1,6)
 
-    def item_use(self, item_type, weapon_damage, potion_heal):
+    def item_use(self, item_type, damage_or_heal):
+        #checks to see if the item we are using is a weapon (Check loot_table.json to see how it is implemented)
         if item_type == "weapon":
-            damage = self.damage_calculation(weapon_damage)
+            #Use the damage_calculation function to deal damage
+            damage = self.damage_calculation(damage_or_heal)
             return damage
         
+        #Similarly to the weapon item type, this checks if the item is a healing potion
         if item_type == "Potion":
-            heal = potion_heal#Will need the value of the healing potion
+            heal = damage_or_heal
             return heal
 
     def damage_calculation(self, weapon): # The damage calculations to determine how much damage the player will do
-        self.roll_20
+        self.roll_20()
 
-        #Enemy Dodge / 0% damage
+        #Dodge / 0% damage
         if self.temproll == 1:
             damage = 0
             self.temproll = 0
@@ -58,22 +66,80 @@ class Combat:
             return 0
     
     def turn_order(self): # Determines the move order based on the 6 sided dice
-        order = False
+        first = None
 
-        while order is False:
-            player_roll = self.roll_6
-            enemy_roll = self.roll_6
+        while first is None:
+            player_roll = self.roll_6()
+            enemy_roll = self.roll_6()
 
             if player_roll > enemy_roll:
                 #player moves first
-                order = True
+                first = "Player"
 
             elif player_roll < enemy_roll:
                 #enemy moves first
-                order = True
+                order = "Enemy"
 
             elif player_roll == enemy_roll:
                 #keep rolling until order is determined
                 pass
 
         return order
+    
+#ADT for mob drops
+class Looting:
+    def __init__(self, loot_table = loot_table): #Made loot_table lt (loot_table.json) a fixed parameter since thats the only loot table I want to pull from
+        self.loot_table = loot_table
+        self.temp_roll = 0
+        self.loot = None
+
+    def roll_100(self): #A 100 sided dice to make rarities easier to distinguish
+        roll = random.randint(1,100)
+        return roll
+
+
+    #What loot any enemy will drop after defeat
+    def enemy_drop(self):
+        enemy_hp = 0 #placeholder until this adt gets implemented
+
+        #When the enemy is dead
+        if enemy_hp == 0:
+            roll = self.roll_100()
+
+            #45% to get a Common item
+            if roll >= 1 and roll <= 45:
+                self.loot = random.choice(self.loot_table["loot"]["common"])#Chooses a random item from the common list
+
+            #35% to get an Uncommon item
+            elif roll >= 46 and roll <= 80:
+                    self.loot = random.choice(self.loot_table["loot"]["uncommon"])#Chooses a random item from the uncommon list
+
+
+            #14% to get a Rare item (Unfortunate part of having a 1% chance somewhere)
+            elif roll >= 81 and roll <= 94:
+                    self.loot = random.choice(self.loot_table["loot"]["rare"])#Chooses a random item from the rare list
+
+            #5% to get an Epic item    
+            elif roll >= 95 and roll <= 99:
+                    self.loot = random.choice(self.loot_table["loot"]["epic"])#Chooses a random item from the epic list
+
+            #1% to get a Legendary item
+            elif roll == 100:
+                    self.loot = random.choice(self.loot_table["loot"]["legendary"])#Chooses a random item from the legendary list
+        
+        #self.temp_roll = 0
+        return self.loot 
+
+
+#Unit tests to see if the Looting class works
+lt = Looting(loot_table)
+
+print(lt.roll_100()) #does not affect the outcome of enemy_drop
+
+drop = lt.enemy_drop()
+
+if drop['type'] == "Weapon": 
+    print(f"You got: {drop['name']} ({drop['rarity']}), Deals: {drop['damage']} damage!")
+
+if drop['type'] == "Potion":
+    print(f"You got: {drop['name']} ({drop['rarity']}), Heals: {drop['heal']} damage!")
